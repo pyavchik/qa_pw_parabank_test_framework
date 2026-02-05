34b6f710390b6502a0e703a618cd298c56635397
Oleksander Pyavchik <pyavchik@gmail.com>
2026-02-05 11:51:59 +0200

    add ParaBank test automation framework
    - Add page objects for auth and account flows
    - Add tests for register, sign-in, forgot login, overview, transfer, bill pay, etc.
    - Organize suites (parentSuite/suite/subSuite) and Allure severity
    - Add auth and account fixtures and parametrized tests

--- src/ui/pages/TransferFundsPage.js
+++ src/ui/pages/TransferFundsPage.js
@@ -1,4 +1,4 @@
-import { expect } from '../../common/helpers/pwHelpers';
+import { expect } from '@playwright/test';
 import { BasePage } from './BasePage';
 
 export class TransferFundsPage extends BasePage {
